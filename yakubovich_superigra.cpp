06badd2f4bed7fc942277da70abcaf6343f3f0e1
popactsa <popactsa.main@yandex.ru>
2023-09-20 22:11:01 +0300

    in process

---
+++ yakubovich_superigra.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <string>  
+
+int guess_counter = 0;
+
+void show(std::vector<char>word, std::vector<bool>revealed){
+    int n = word.size();
+    char blank = '-';
+    for (int i = 0; i < n; ++i){
+        if(revealed[i] == true){
+            std::cout<<word[i]<<' ';
+        }
+        else{
+            std::cout<<blank<<' ';
+        }
+    }
+    std::cout<<std::endl<<std::endl;
+}
+
+bool finder_letter(std::vector<char>word, char let, std::vector<bool>revealed){
+    int n = word.size();
+    bool status = false;
+    for (int i = 0; i < n; ++i){
+        if (word[i] == let && revealed[i] == false){
+            revealed[i] = true;
+            status = true;
+            guess_counter++;
+        }
+        else if(word[i] == let && revealed[i] == true){
+            status = 0;
+            continue;
+        }
+    }
+    return status;
+}
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+    std::string word_ = "moloko";
+    std::vector<char>word;
+    for(char i : word_){
+        word.push_back(i);
+    }
+    int wsize = word.size();
+    std::vector<bool>revealed(wsize, 0);
+    int status = 1;
+    char let;
+    bool f; 
+    while(status == 1){
+        std::cout<<"Ваша буква?"<<std::endl;
+        std::cin>>let;
+        f = finder_letter(word, let, revealed);
+
+        if (f == 0){
+            std::cout<<std::endl<<"Да что Вы!"<<std::endl;
+        }
+        else{
+            std::cout<<std::endl<<"Откройте букву!"<<std::endl;
+        }
+        show(word, revealed);
+        if (guess_counter == wsize){
+            status = 0;
+            std::cout<<"Вы выиграли!"<<std::endl;
+        }
+    }
+}
