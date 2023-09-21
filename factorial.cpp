8e7aea11779bd784b1c3e7365c0e99ff23c8740d
popactsa <popactsa.main@yandex.ru>
2023-09-21 16:25:13 +0300

    shepelev_2

---
+++ factorial.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+unsigned long long factorial(unsigned long long num){
+    unsigned long long res = 1;
+    for (unsigned long long i = 1; i <= num; ++i){
+        res *= i;
+    }
+    return res;
+}
+
+int main(){
+    std::string num_entered;
+    entry:
+    std::cin>>num_entered;
+    std::cout<<std::endl;
+    for(auto i : num_entered){
+        if(i < '0' ^ i > '9'){
+            std::cout<<"unappropriate value"<<std::endl;
+            goto entry;
+        }
+    }
+    unsigned long long num = stoi(num_entered);
+    if (num > 1000){
+        std::cout<<"a value is too big"<<std::endl;
+        goto entry;
+    }
+    std::cout<<num<<std::endl;
+    std::cout<<factorial(num);
+    return 0;
+}
---
+++ fibonacci.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+void fibonacci(unsigned int num){
+    unsigned int res = 1;
+    unsigned int prev = 0;
+    unsigned int temp = 0;
+
+    std::cout<<temp<<' ';
+    for (unsigned long long i = 0; i <= num-2; ++i){
+        std::cout<<res<<' ';
+        temp = res;
+        res += prev;
+        prev = temp;
+    }
+}
+
+int main(){
+    std::string num_entered;
+    entry:
+    std::cin>>num_entered;
+    std::cout<<std::endl;
+    for(auto i : num_entered){
+        if(i < '0' ^ i > '9'){
+            std::cout<<"unappropriate value"<<std::endl;
+            goto entry;
+        }
+    }
+    unsigned int num = stoi(num_entered);
+    if (num > 100 ^ num == 0){
+        std::cout<<"incorrect value"<<std::endl;
+        goto entry;
+    }
+    std::cout<<num<<std::endl;
+    fibonacci(num);
+    return 0;
+}
